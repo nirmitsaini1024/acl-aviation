3569bbbbb83ba9994373254bc2ec6a1346af847f
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-05-29 13:00:02 +0530

    add modules

--- backend-acl/src/app.module.ts
+++ backend-acl/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { AuthModule } from './auth/auth.module';
+import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
-  imports: [],
+  imports: [RolesModule, UsersModule, GroupsModule, PermissionsModule, AuthModule, TenantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
