ba5a0c4283ab1a7f812a0c61f9561f21471d6721
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-05-31 01:08:13 +0530

    modify role module

--- backend-acl/src/roles/roles.module.ts
+++ backend-acl/src/roles/roles.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
-import { RolesService } from './roles.service';
 import { RoleSchema } from './schemas/role.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RoleService } from './roles.service';
 
 @Module({
   imports : [ MongooseModule.forFeature([{ name: 'Role', schema: RoleSchema }]),],
   controllers: [RolesController],
-  providers: [RolesService]
+  providers: [RoleService]
 })
 export class RolesModule {}
