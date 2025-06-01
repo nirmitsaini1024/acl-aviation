9e3ff652152a0b3f8128c81f1fcbb855943bfff2
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-06-01 14:15:27 +0530

    auth jwt

--- aviation/src/utils/auth.js
+++ aviation/src/utils/auth.js
@@ -45,6 +45,7 @@ export const getTenantId = async () => {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`
       },
+      
       body: JSON.stringify({
         tenantName: 'Default Tenant',
         userName: 'Admin',
